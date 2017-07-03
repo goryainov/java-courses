b23bd2e0f233e26eff8d856dc36aa5daf10d298f
goryainov.vn <goryainov.vn@gmail.com>
2017-07-04 00:19:20 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,17 @@ public class Calculate {
 
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		short sum = (short) (first + second);
+		double div = (double)first / second;
+		float dec = (float) first - second;
+		int mult = first * second;
+		long power = (long) Math.pow(first, second);
+		System.out.println("sum = " + sum);
+		System.out.println("dec = " + dec);
+		System.out.println("mult = " + mult);
+		System.out.println("div = " + div);
+		System.out.println("power = " + power);
 	}
 }
